4a91495f5d405dfedccc419a2339a2b4ad029a08
ft85 <35577034+ft85@users.noreply.github.com>
2026-01-14 16:20:41 +0200

    Update login Page design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -558,24 +558,7 @@
 
         <!-- Demo Credentials -->
         <!-- <div class="demo-card">
-            <div class="demo-header">
-                <div class="demo-icon">
-                    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M13 16H12V12H11M12 8H12.01M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div>
-                <div class="demo-title">Demo Credentials</div>
-            </div>
-            <div class="demo-credentials">
-                <div class="demo-row">
-                    <span class="demo-label">Email:</span>
-                    <span class="demo-value">admin@airlineagency.com</span>
-                </div>
-                <div class="demo-row">
-                    <span class="demo-label">Password:</span>
-                    <span class="demo-value">password123</span>
-                </div>
-            </div>
+            
         </div> -->
 
         <!-- Footer -->
